306093f38ef7672aa81432dd647268a3fcc4760d
Şenol AKKAŞ <senolakkas@gmail.com>
2017-04-13 00:13:17 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,117 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 // variables
-var isProduction = process.argv.indexOf('-p') >= 0;
-var sourcePath = path.join(__dirname, './src');
-var outPath = path.join(__dirname, './dist');
+const isProduction = process.argv.indexOf('-p') >= 0;
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './dist');
 
 // plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: sourcePath,
-  entry: {
-    main: './index.ts',
-    vendor: [
-      'react',
-      'react-dom',
-      'react-router',
-      'mobx',
-      'mobx-react',
-      'mobx-react-router'
-    ]
-  },
-  output: {
-    path: outPath,
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  target: 'web',
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-    // Fix webpack's default behavior to not load packages with jsnext:main module
-    // (jsnext:main directs not usually distributable es6 format, but es6 sources)
-    mainFields: ['module', 'browser', 'main']
-  },
-  module: {
-    loaders: [
-      // .ts, .tsx
-      {
-        test: /\.tsx?$/,
-        use: isProduction
-          ? 'awesome-typescript-loader?module=es6'
-          : [
-            'react-hot-loader',
-            'awesome-typescript-loader'
-          ]
-      },
-      // css 
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              query: {
-                modules: true,
-                sourceMap: !isProduction,
-                importLoaders: 1,
-                localIdentName: '[local]__[hash:base64:5]'
-              }
-            },
-            {
-              loader: 'postcss-loader'
-            }
-          ]
-        })
-      },
-      // static assets 
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.png$/, use: 'url-loader?limit=10000' },
-      { test: /\.jpg$/, use: 'file-loader' },
-    ],
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        context: sourcePath,
-        postcss: [
-          require('postcss-import')({ addDependencyTo: webpack }),
-          require('postcss-url')(),
-          require('postcss-cssnext')(),
-          require('postcss-reporter')(),
-          require('postcss-browser-reporter')({ disabled: isProduction }),
+    context: sourcePath,
+    entry: {
+        main: './index.ts',
+        vendor: [
+            'react',
+            'react-dom',
+            'react-router',
+            'mobx',
+            'mobx-react',
+            'mobx-react-router'
         ]
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.bundle.js',
-      minChunks: Infinity
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      disable: !isProduction
-    }),
-    new HtmlWebpackPlugin({
-      template: 'assets/index.html'
-    })
-  ],
-  devServer: {
-    contentBase: sourcePath,
-    hot: true,
-    stats: {
-      warnings: false
     },
-  },
-  node: {
-    // workaround for webpack-dev-server issue 
-    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
-    fs: 'empty',
-    net: 'empty'
-  }
+    output: {
+        path: outPath,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+        // Fix webpack's default behavior to not load packages with jsnext:main module
+        // (jsnext:main directs not usually distributable es6 format, but es6 sources)
+        mainFields: ['module', 'browser', 'main']
+    },
+    module: {
+        loaders: [
+            // .ts, .tsx
+            {
+                test: /\.tsx?$/,
+                use: isProduction
+                    ? 'awesome-typescript-loader?module=es6'
+                    : [
+                        'react-hot-loader',
+                        'awesome-typescript-loader'
+                    ]
+            },
+            // css
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            query: {
+                                modules: true,
+                                sourceMap: !isProduction,
+                                importLoaders: 1,
+                                localIdentName: '[local]__[hash:base64:5]'
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader'
+                        }
+                    ]
+                })
+            },
+            // static assets
+            { test: /\.html$/, use: 'html-loader' },
+            { test: /\.png$/, use: 'url-loader?limit=10000' },
+            { test: /\.jpg$/, use: 'file-loader' },
+        ],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                context: sourcePath,
+                postcss: [
+                    require('postcss-import')({ addDependencyTo: webpack }),
+                    require('postcss-url')(),
+                    require('postcss-cssnext')(),
+                    require('postcss-reporter')(),
+                    require('postcss-browser-reporter')({ disabled: isProduction }),
+                ]
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.bundle.js',
+            minChunks: Infinity
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new ExtractTextPlugin({
+            filename: 'styles.css',
+            disable: !isProduction
+        }),
+        new HtmlWebpackPlugin({
+            template: 'assets/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: sourcePath,
+        hot: true,
+        stats: {
+            warnings: false
+        },
+    },
+    node: {
+        // workaround for webpack-dev-server issue
+        // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+        fs: 'empty',
+        net: 'empty'
+    }
 };
