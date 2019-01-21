bdfa3b184b14f9c0fe40718359dfdd3454e49ce2
Youngrok Kim <rok0810@gmail.com>
2019-01-21 14:23:05 +0900

    Replace older hot decorator from react-hot-loader

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 import { Router, Route, Switch } from 'react-router';
 import { Root } from 'app/containers/Root';
 import { TodoApp } from 'app/containers/TodoApp';
 
 // render react DOM
-export const App = hot(module)(({ history }) => (
+export const App = hot(({ history }) => (
   <Root>
     <Router history={history}>
       <Switch>
